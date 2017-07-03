d521953584fe0c6ecc6af0121ca46399cd9ffb3c
Shika Shyam <shika.shyam2014@vit.ac.in>
2017-07-03 10:12:00 +0400

    Integrated Twitter but irrelevance of tweets not fixed

---
+++ demo/src/main/java/com/google/android/exoplayer2/demo/AndroidVersion.java
@@ -0,0 +1,15 @@
+package com.google.android.exoplayer2.demo;
+
+/**
+ * Created by ADMIN on 01-07-2017.
+ */
+
+public class AndroidVersion {
+
+    private String tweet;
+
+    public String getVer() {
+        return tweet;
+    }
+
+}
---
+++ demo/src/main/java/com/google/android/exoplayer2/demo/Response.java
@@ -0,0 +1,17 @@
+package com.google.android.exoplayer2.demo;
+
+/**
+ * Created by ADMIN on 01-07-2017.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Response {
+
+    private List<AndroidVersion> android = new ArrayList<AndroidVersion>();
+
+    public List<AndroidVersion> getAndroid() {
+        return android;
+    }
+}
